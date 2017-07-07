f55aeb85376ffbf791fcf1ccb573f0fdd41d8516
Andrea N <andynsn@me.com>
2017-07-07 09:05:52 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,20 +2,28 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+In this function I set the inverse as NULL because it needs to be  a placeholder for a future value and I set the function of the matrix X to a new matrix c
 makeCacheMatrix <- function(x = matrix()) {
 	inv <- NULL
 	set<- function(y) {
 		x <<- y
 		inv <<- NULL
+		}
+	 get <- function() x
+	 setInverse <- function(solve)
+		 inv <<- solve
+	 getInverse <- function() inv
+		   
+	list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
-In this function I set the inverse as NULL because it needs to be  a placeholder for a future value and I setted the function of the matrix X to a new matrix called Y, getting the inverse for this new matrix. 
-
 ## Write a short comment describing this function
 
+What I tried to do with this function was use the matrix returned by the last function and only if that matrix has not changed and the inverse has already been calculated then cacheSolve function should give back the inverse of the cache itself. 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-      inv<- x$getInverse()
+      inv <- x$getInverse()
       if(!is.null(inv)) {
       	message ("Getting the data cached")
       	return(inv)
@@ -25,4 +33,3 @@ cacheSolve <- function(x, ...) {
 	x$setInverse(inv)
 	inv
 }
-What I tried to do with this function was use the matrix returned by the last function and only if that matrix has not changed and the inverse has already been calculated then cacheSolve function should give back the inverse of the cache itself. 
